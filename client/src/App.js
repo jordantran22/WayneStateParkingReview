73418e22c3ccc90e14114c9babdc013d3d652d63
Jordan Tran <jordan22tran@gmail.com>
2022-03-01 14:41:15 -0500

    created parking structure page component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/HomePage';
-import { Route, Link } from 'react-router-dom'
+// import { BrowserRouter as Router, Switch, Route } from 'history/createBrowserHistory';
 
 function App() {
   return (
-    <div className="App">
-      {/* <Route exact path="/components/HomePage.js" component={<HomePage />} /> */}
-      {/* // <HomePage/> */}
-      <HomePage />
-    </div>
+      <div className="App">
+        <HomePage />
+      </div>
   );
 }
 
---
+++ client/src/components/ParkingStructurePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ParkingStructurePage = () => {
+  return (
+    <div>ParkingStructurePage</div>
+  )
+}
+
+export default ParkingStructurePage
