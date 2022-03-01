1fc364e4df2ebbb3bdb29dd7ac50d2f9774531f3
Jordan Tran <jordan22tran@gmail.com>
2022-03-01 17:04:12 -0500

    created sign up popup

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,8 +1,11 @@
 import React from 'react';
 // import wsu_logo from '../images/wsuLogo.png';
 import SearchBar from './SearchBar';
+import { useState } from 'react';
 
 const NavBar = () => {
+    const [signInClicked, setSignInClicked] = useState(false);
+
     return (
         <div>
 
@@ -17,11 +20,42 @@ const NavBar = () => {
                         <SearchBar />
                     </li>
 
-                    <li className="nav-item ff-condensed fw-bold">
+                    <li className="nav-item ff-condensed fw-bold" onClick={() => setSignInClicked(true)}>
                         Sign In
                     </li>
                 </ul>
             </div>
+
+            {
+                signInClicked &&
+                <div className="loginPopupContainer">
+                    <div className="loginPopupForm">
+                            <div className="closeX" onClick={() => setSignInClicked(false)}>
+                                X
+                            </div>
+                            <div>
+                                <img className="wsulogo" src="https://media2.metrotimes.com/metrotimes/imager/u/original/6344372/x4pckygq_400x400.jpg" />
+                            </div>
+                            <div>
+                                <h2>WSU Parking Review!</h2>
+                                <div>Enter Email Address</div>
+                                <div>
+                                    <input className="inputForLogin" type="text" placeholder='Enter Email Address'></input>
+                                </div>
+                         
+                                <div>Enter Password</div>
+                                <div>
+                                    <input className="inputForLogin" type="password" placeholder='Enter Password'></input>
+                                </div>
+
+                                <button className="loginButton">Login</button>
+
+                                <div>Don't have an account? </div>
+                                <button className="signupButton">Sign Up!</button>
+                            </div>
+                    </div>  
+                </div>
+            }
 
 
         </div>
