ebd7038fd0cc7acd53abcd51300b0e2bab043505
Jordan Tran <jordan22tran@gmail.com>
2022-03-10 10:13:45 -0500

    Update reviews.sql

--- server/dbChanges/reviews.sql
+++ server/dbChanges/reviews.sql
@@ -4,6 +4,7 @@ user_id INT(6) NOT NULL,
 parking_structure_id INT(6) NOT NULL,
 review_text VARCHAR(300) NOT NULL,
 review_rating INT(6) NOT NULL,
+review_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 PRIMARY KEY (review_id),
 KEY(user_id),
 KEY(parking_structure_id)
