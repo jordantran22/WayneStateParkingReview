1bc7fd302d774765e0ef0f4da4fb126d52141c47
slattery-mark <markys1@hotmail.com>
2022-03-13 11:51:02 -0400

    added ids to structure buttons

--- client/src/components/QuickViewCard.js
+++ client/src/components/QuickViewCard.js
@@ -43,7 +43,7 @@ const QuickViewCard = ({ structure, loggedInStatus}) => {
 
 
     return (
-        <button className="quick-view-card" id={structure.name} onClick={() => navigateToStructureDetailsPage()}>
+        <button className="quick-view-card" id={structure.id} onClick={() => navigateToStructureDetailsPage()}>
             <img src={structure.image} />
             <div className="quick-view-card__info">
                 <h2>Parking Structure {structure.number}</h2>
