27e1e9e189e298bc7e122289a9e2d55f0143759f
slattery-mark <markys1@hotmail.com>
2022-03-13 13:56:06 -0400

    fixed bugs related to list keys and table format

--- client/src/components/DetailCard.js
+++ client/src/components/DetailCard.js
@@ -9,14 +9,14 @@ const DetailCard = ({ parkingStructureInfo, totalReviews, structureRate }) => {
     }, [structureRate]);
 
     return (
-        <div>
+        <div key={parkingStructureInfo.name}>
             <h1>{parkingStructureInfo.name}</h1>
             <div className='rating'>
                 <ReactStars color2={"#FDC741"} color1={"#E5E5E5"} count={5} size={30} edit={false} value={structureRate} />
                 <span>{totalReviews} reviews</span>
             </div>
             <div className="detail-card">
-                <div class="detail-card__info">
+                <div className="detail-card__info">
                     <a href="https://goo.gl/maps/MCUmvVbq1EpstMFc7" target="_blank">
                         <img src={parkingStructureInfo.image} />
                     </a>
@@ -25,42 +25,44 @@ const DetailCard = ({ parkingStructureInfo, totalReviews, structureRate }) => {
                         {parkingStructureInfo.address}
                     </a>
                 </div>
-                <table class="detail-card__hours">
-                    <tr>
-                        <th className='current-day'>Mon</th>
-                        <td>12:00 a.m. - 12:00 a.m.</td>
-                        <td className='green-span'>Open</td>
-                    </tr>
-                    <tr>
-                        <th>Tue</th>
-                        <td>12:00 a.m. - 12:00 a.m.</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <th>Wed</th>
-                        <td>12:00 a.m. - 12:00 a.m.</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <th>Thu</th>
-                        <td>12:00 a.m. - 12:00 a.m.</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <th>Fri</th>
-                        <td>12:00 a.m. - 12:00 a.m.</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <th>Sat</th>
-                        <td>12:00 a.m. - 12:00 a.m.</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <th>Sun</th>
-                        <td>12:00 a.m. - 12:00 a.m.</td>
-                        <td></td>
-                    </tr>
+                <table className="detail-card__hours">
+                    <tbody>
+                        <tr>
+                            <th className='current-day'>Mon</th>
+                            <td>12:00 a.m. - 12:00 a.m.</td>
+                            <td className='green-span'>Open</td>
+                        </tr>
+                        <tr>
+                            <th>Tue</th>
+                            <td>12:00 a.m. - 12:00 a.m.</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th>Wed</th>
+                            <td>12:00 a.m. - 12:00 a.m.</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th>Thu</th>
+                            <td>12:00 a.m. - 12:00 a.m.</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th>Fri</th>
+                            <td>12:00 a.m. - 12:00 a.m.</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th>Sat</th>
+                            <td>12:00 a.m. - 12:00 a.m.</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th>Sun</th>
+                            <td>12:00 a.m. - 12:00 a.m.</td>
+                            <td></td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
         </div>
